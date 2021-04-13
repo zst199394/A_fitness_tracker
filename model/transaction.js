bb5561721d8abb9fdd05188977df0a7e40d5156a
shuting <zst9394@outlook.com>
2021-04-13 13:31:37 -0400

    change models done

---
+++ model/transaction.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const fitnessSchema = new Schema({
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter excercise name",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter excercise type",
+      },
+      duration: {
+        type: Number,
+        required: "Enter an amount",
+      },
+      distance: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+    },
+  ],
+
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Fitness = mongoose.model("Fitness", fitnessSchema);
+
+module.exports = Fitness;
